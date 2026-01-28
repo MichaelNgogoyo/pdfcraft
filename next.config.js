6cd7ba565d74ebbea933c9178ede88ecc3bc4bde
Ngogoyo Michael <michaeln1486@gmail.com>
2026-01-28 23:26:06 +0300

    Disable static export mode in Next.js config

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Enable static export for deployment flexibility
-  output: 'export',
+  // output: 'export',
 
   // Webpack configuration for WASM modules
   webpack: (config, { isServer, webpack }) => {
